ce7b50b89ba959b91a58bba51dc2eac6b06f6ad2
Michael Rubanov <mrubanov@gmail.com>
2014-10-07 09:24:39 +0300

    fisxing spaces and docsstrings

--- src/mr_clj_general/core.clj
+++ src/mr_clj_general/core.clj
@@ -4,48 +4,57 @@
 
 (defn aprint aprint.core/aprint)
 
-(defn modify-keys 
+(defn modify-keys
   "Modify keys of map , convert them using function f"
-  [f m] (zipmap (map f (keys m)) (vals m)))
+  [f m]
+  (zipmap (map f (keys m)) (vals m)))
 
-(defn change-keys-to-keywords 
+(defn change-keys-to-keywords
   "Convert map keys to keywords"
-  [map-data] (modify-keys keyword map-data))
+  [map-data]
+  (modify-keys keyword map-data))
 
-(defn args-to-str [ args ]
+(defn args-to-str
   "Contactination of args separated by space"
+  [ args ]
   (clojure.string/join " " args))
 
-(defn error [ & args]
+(defn error
   "Logs error"
+  [ & args]
   (log/error (args-to-str args)))
 
-(defn info [ & args]
+(defn info
   "Logs info"
+  [ & args]
   (log/info (args-to-str args)))
 
-(defn debug [ & args]
-  "Logs info"
+(defn debug
+  "Logs debug"
+  [ & args]
   (log/debug (args-to-str args)))
 
-(defn third 
+(defn third
   "Get thrid vector's element"
-  [vect] (get vect 2))
+  [vect]
+  (get vect 2))
 
-(defn indexof 
+(defn indexof
   "Index of value in vector v"
   [value v]
   (.indexOf v value ))
 
-(defn find-position [v value]
+(defn find-position
   "Find position of value in vector v"
+  [v value]
   (filter
     #(when (second %) %)
     (keep-indexed vector
                   (map #(identity (= value (second %)))
                        (keep-indexed vector v)))))
 
-(defn find-positions [values v]
+(defn find-positions
   "Find position of each value in vector values , in vector v."
+  [values v]
   (map first (map first (map (partial find-position v) values))))
 
